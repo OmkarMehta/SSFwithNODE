11568b2a15e9beeed02b42196a644e4314e3567e
OmkarMehta <mehta.omkar@gmail.com>
2021-08-18 15:12:34 +0000

    Added np.allclose link

--- pyddm-source-code/ddm/sample.py
+++ pyddm-source-code/ddm/sample.py
@@ -110,6 +110,7 @@ class Sample(object):
         """Iterate through each reaction time, with no regard to whether it was a correct or error trial."""
         return np.concatenate([self.corr, self.err]).__iter__()
     def __eq__(self, other):
+        # https://numpy.org/doc/stable/reference/generated/numpy.allclose.html
         if not np.allclose(self.corr, other.corr) or \
            not np.allclose(self.err, other.err) or \
            self.undecided != other.undecided:
