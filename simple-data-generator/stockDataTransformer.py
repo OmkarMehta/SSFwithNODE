f7e4633f19356e785db57108217aaffad6c46036
OmkarMehta <mehta.omkar@gmail.com>
2021-09-06 15:01:16 +0000

    stock data collector and transformer done

---
+++ simple-data-generator/stockDataTransformer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+def stockDataTransformer(filepath):
+    df = pd.read_csv(filepath)
+    df.set_index('Date', inplace=True)
+    df1 = df[['Open', 'Close']].copy()
+    data = df1.values
+    n_samples = data.shape[0]//10*10
+    reshape_number = data.shape[0]*data.shape[1]//10
+    data1 = data[:n_samples].reshape((reshape_number, 10))
+    return data1
+tickerName = 'Apple'
+filepath = f"raw-stock-data/{tickerName}.csv"
