77d51a5e78811c62d3484d1da7b6ccd80495379b
OmkarMehta <mehta.omkar@gmail.com>
2021-09-18 16:23:09 +0000

    Changed stockDataTransformer

--- simple-data-generator/stockDataTransformer.py
+++ simple-data-generator/stockDataTransformer.py
@@ -6,7 +6,7 @@ def stockDataTransformer(filepath):
     df1 = df[['Open', 'Close']].copy()
     data = df1.values
     n_samples = data.shape[0]//10*10
-    reshape_number = data.shape[0]*data.shape[1]//10
+    reshape_number = n_samples*data.shape[1]//10
     data1 = data[:n_samples].reshape((reshape_number, 10))
     return data1
 tickerName = 'Apple'
